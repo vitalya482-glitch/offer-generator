a8a2ecf5d3e51f01285d7bffac54f8798758311a
vitalya482-glitch <vitalya482@gmail.com>
2026-08-14 15:29:48 +0500

    Use unified STULZ compressor runtime

--- brands/registry.py
+++ brands/registry.py
@@ -5,7 +5,7 @@ from types import ModuleType
 
 
 BRANDS = {
-    "Stulz": "brands.stulz_cable_qty_runtime",
+    "Stulz": "brands.stulz_compressor_runtime",
     "Riello": "brands.riello",
     "DC Eltek": "brands.dc_eltek",
     "Generator": "brands.generator",
