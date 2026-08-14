81b7b89adceaf166eb956f31396fd45df4faba58
vitalya482-glitch <vitalya482@gmail.com>
2026-08-14 12:09:01 +0500

    Route STULZ through legend runtime

--- brands/registry.py
+++ brands/registry.py
@@ -5,7 +5,7 @@ from types import ModuleType
 
 
 BRANDS = {
-    "Stulz": "brands.stulz_ui_runtime",
+    "Stulz": "brands.stulz_legend_runtime",
     "Riello": "brands.riello",
     "DC Eltek": "brands.dc_eltek",
     "Generator": "brands.generator",
